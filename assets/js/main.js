8e043f0c6db2e30ca4c5f1cb0fc06f64167149f3
Marianny Díaz <mariianniidiiaz@gmail.com>
2022-09-17 10:39:45 -0500

    Ejercicio terminado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,14 @@
 console.log("Entro al main.js");
+
+const imgRandom = document.getElementById("imgRandom");
+const URL = "https://picsum.photos/200/500";
+
+const randomImage = () => {
+    console.log("Entro a la función randomImage");
+    fetch(URL)
+        .then((data) => {
+            console.log(data);
+            imgRandom.src = data.url;
+        })
+        .catch((error) => console.log("¡Ocurrió un error!"));
+};
